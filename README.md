57a93dde03235376bdcbda1df854e700361dca9a
kellymersereau <kelly.mersereau@gmail.com>
2016-05-16 19:15:20 -0400

    updates

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# Smashing_Aces
+#Smashing Aces!          
+
+
+###Synopsis:
+
+An interactive 
+
+ Pitch the idea in three to four sentences.
+
+
+
+###Contributors:  
+---
+
+* David Daskal [GitHub](https://github.com/DavidDaskal)  
+* Kelly Mersereau [GitHub](https://github.com/kellymersereau)
+* Kevin Arnold [GitHub](https://github.com/Kevarnold02)
+
+
+###Technologies used:
+---
+* HTML
+* CSS
+ * BootStrap / Materialize / Skeleton Grid
+* JavaScript
+ * jQuery
+ * AJAX
+* [Trello](https://trello.com/smashingaces)
+* Mindjet Mind Manager
+* Node.JS
+  * Express
+  * Handlebars
+  * Sequelize
+  * NPM Package Control
+* MySQL
+* Socket.io
+
+
+###APIs
+---
+* Primary:
+ * API One
+ * API Two
+ * API Three
+* Secondary:
+ * JSfiddle
+ * Google Search
+
+
+###Code We're proud of
+---
+Ideally a bit of code that you really liked or something interesting from the Project (fill out this section after you're finished
+
+
+
+
+###License
+---
+Copyright 2009-2016 Rutgers Coding Bootcamp.
---
+++ db/seeds.sql
@@ -0,0 +1,57 @@
+#Schema to set up info for each individual card
+
+INSERT INTO cards (suite,rank,image_link) VALUES ('spades',2,'s02.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('spades',3,'s03.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('spades',4,'s04.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('spades',5,'s05.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('spades',6,'s06.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('spades',7,'s07.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('spades',8,'s08.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('spades',9,'s09.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('spades',10,'s10.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('spades',11,'s11.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('spades',12,'s12.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('spades',13,'s13.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('spades',14,'s14.bmp');
+
+INSERT INTO cards (suite,rank,image_link) VALUES ('clubs',2,'c02.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('clubs',3,'c03.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('clubs',4,'c04.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('clubs',5,'c05.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('clubs',6,'c06.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('clubs',7,'c07.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('clubs',8,'c08.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('clubs',9,'c09.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('clubs',10,'c10.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('clubs',11,'c11.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('clubs',12,'c12.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('clubs',13,'c13.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('clubs',14,'c14.bmp');
+
+INSERT INTO cards (suite,rank,image_link) VALUES ('diamonds',2,'d02.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('diamonds',3,'d03.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('diamonds',4,'d04.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('diamonds',5,'d05.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('diamonds',6,'d06.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('diamonds',7,'d07.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('diamonds',8,'d08.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('diamonds',9,'d09.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('diamonds',10,'d10.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('diamonds',11,'d11.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('diamonds',12,'d12.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('diamonds',13,'d13.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('diamonds',14,'d14.bmp');
+
+INSERT INTO cards (suite,rank,image_link) VALUES ('hearts',2,'h02.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('hearts',3,'h03.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('hearts',4,'h04.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('hearts',5,'h05.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('hearts',6,'h06.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('hearts',7,'h07.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('hearts',8,'h08.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('hearts',9,'h09.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('hearts',10,'h10.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('hearts',11,'h11.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('hearts',12,'h12.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('hearts',13,'h13.bmp');
+INSERT INTO cards (suite,rank,image_link) VALUES ('hearts',14,'h14.bmp');
