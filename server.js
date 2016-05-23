c10065369440914a5245c0263342b643bcd325c9
kellymersereau <kelly.mersereau@gmail.com>
2016-05-23 08:51:09 -0400

    removed waiting room controller - not needed

--- server.js
+++ server.js
@@ -26,13 +26,13 @@ var application_controllers = require('./controllers/application_controllers.js'
 var cards_controller = require('./controllers/cards_controller.js');
 var user_controller = require('./controllers/user_controller.js');
 var game_controller = require('./controllers/game_controller.js');
-// var waitingRoom_controller = require('./controllers/waitingRoom_controller.js');
+var waitingRoom_controller = require('./controllers/waitingRoom_controller.js');
 
 app.use('/', application_controllers);
 app.use('/cards', cards_controller);
 app.use('/user', user_controller);
 app.use('/game', game_controller);
-// app.use('/waitingRoom', waitingRoom_controller);
+app.use('/waitingRoom', waitingRoom_controller);
 
 var port = process.env.PORT || 3000;
 app.listen(port);
