a74253768f251f744d1f1eb78aa04a6a371ec383
DavidDaskal <daskaldv@gmail.com>
2016-05-27 23:52:09 -0400

    edit connection

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,25 @@
 Here is where you make the connection to the database and export and used by the O.R.M.
 */
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    // port: 3000,
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'cards_db'
-});
+var connection;
+// = mysql.createConnection({
+//     // port: 3000,
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'cards_db'
+// });
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+    connection = mysql.createConnection({
+        port: 3000,
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'cards_db'
+    });
+};
 
 connection.connect(function(err) {
     if (err) {
@@ -17,7 +29,5 @@ connection.connect(function(err) {
     }
     console.log('connected as id ' + connection.threadId);
 });
-
-var sess;
 
 module.exports = connection;
