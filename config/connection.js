9051874a0368bb49a3825444da0aeb3cafd6db69
kellymersereau <kelly.mersereau@gmail.com>
2016-05-28 10:26:37 -0400

    just fixed the way win/loss looks

--- config/connection.js
+++ config/connection.js
@@ -2,25 +2,24 @@
 Here is where you make the connection to the database and export and used by the O.R.M.
 */
 var mysql = require('mysql');
-var connection;
-// = mysql.createConnection({
-//     // port: 3000,
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'cards_db'
-// });
-if(process.env.JAWSDB_URL){
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else{
-    connection = mysql.createConnection({
-        port: 3000,
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'cards_db'
-    });
-};
+var connection = mysql.createConnection({
+    // port: 3000,
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'cards_db'
+});
+// if(process.env.JAWSDB_URL){
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else{
+//     connection = mysql.createConnection({
+//         port: 3000,
+//         host: 'localhost',
+//         user: 'root',
+//         password: '',
+//         database: 'cards_db'
+//     });
+// };
 
 connection.connect(function(err) {
     if (err) {
